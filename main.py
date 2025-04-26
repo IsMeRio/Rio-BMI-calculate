500d699d36b46b6f4b7a7bbefeb95027159eb330
Rio <111352746+IsMeRio@users.noreply.github.com>
2025-04-26 18:52:16 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
 import streamlit as st
 
-st.title("BMI Calculator")
-imgshow=st.empty()
-bmishow=st.empty()
-categoryshow=st.empty()
+st.set_page_config(initial_sidebar_state="collapsed")
 
-bmi_categories = {
-    18.5: "Underweight",
-    24.9: "Healthy weight",
-    29.9: "Overweight",
-    float('inf'): "Obese"
-}
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
-Gender = st.radio("Select Gender:", options=["Male", "Female"],horizontal=True)
-kg = st.slider("Weight (Kg)", 0, 500)
-height = st.slider("Height (Cm)", 0, 500)
+st.markdown("<h1 style='text-align: center;'>IsMeRio Main Page</h1>", unsafe_allow_html=True)
 
-if kg > 0 and height > 0:
-    imgnum = 0
-    bmi = kg / ((height / 100) ** 2)
-    bmi_str = f"{bmi:.2f}"
-
-    for threshold, category in bmi_categories.items():
-        imgnum += 1
-        if bmi <= threshold:
-            result = category
-            break
-
-    bmishow.write(f"BMI: {bmi_str}")
-    categoryshow.write(f"Category: **{result}**")
-
-    image_path = f"img/{Gender.lower()}{imgnum}.png"
-    imgshow.image(image_path,width=250)
-
-else:
-    if kg == 0 and height > 0:
-        st.header("Please enter valid weight.")
-    elif kg > 0 and height == 0:
-        st.header("Please enter valid height.")
-    else:
-        st.header("Please enter valid weight and height.")
+st.page_link("pages/bmi.py",label="BMI Calculate",)
